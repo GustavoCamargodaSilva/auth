bf1eff0d026424792744778f5f0e51e421a65c4d
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2024-10-19 19:33:57 -0300

    conexao banco de dados

---
+++ stateless/stateless-auth-api/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.user (id, username, password) VALUES (1, 'admin', '$2y$10$EDogeE1XDcIT7nB0Yji6h.KcqoE.JoGtXzkgQxZeHWoMFVxkJvYYO');
+
+INSERT INTO public.user (id, username, password) VALUES (2, 'teste', '$2y$10$EDogeE1XDcIT7nB0Yji6h.KcqoE.JoGtXzkgQxZeHWoMFVxkJvYYO');
